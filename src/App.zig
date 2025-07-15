b87d215a4b7df566c07b16122be316a40d822f5a
Emmmmllll <gocki16@gmail.com>
2025-07-15 14:08:24 +0200

    Put app state into App.zig

---
+++ src/App.zig
@@ -0,0 +1,131 @@
+const std = @import("std");
+const rl = @import("raylib");
+const notes = @import("notes.zig");
+const TonnetzGrid = @import("TonnetzGrid.zig");
+const Audio = @import("Audio.zig");
+
+const App = @This();
+
+last_click: i64 = 0,
+last_clicked: ?notes.Key = null,
+double_click_delay: i64 = 500,
+grid: TonnetzGrid,
+key_map: [notes.Key.max + 1]struct { kb: rl.KeyboardKey, note: notes.Key, state: enum { off, kb, toggle } = .off } = .{
+    .{ .kb = .z, .note = .c },
+    .{ .kb = .s, .note = .db },
+    .{ .kb = .x, .note = .d },
+    .{ .kb = .d, .note = .eb },
+    .{ .kb = .c, .note = .e },
+    .{ .kb = .v, .note = .f },
+    .{ .kb = .g, .note = .@"f#_gb" },
+    .{ .kb = .b, .note = .g },
+    .{ .kb = .h, .note = .ab },
+    .{ .kb = .n, .note = .a },
+    .{ .kb = .j, .note = .bb },
+    .{ .kb = .m, .note = .b },
+},
+audio: *Audio,
+current_wave: Audio.Wave = .sine,
+
+pub fn deinit(self: *App) void {
+    self.audio.deinit();
+}
+
+pub fn handleInput(self: *App) void {
+    const audio = self.audio;
+    if (rl.isKeyPressed(.w)) {
+        self.current_wave = switch (self.current_wave) {
+            .sine => .square,
+            .square => .triangle,
+            .triangle => .sawtooth,
+            .sawtooth => .sine,
+        };
+    }
+
+    for (&self.key_map) |*key_pair| {
+        if (key_pair.state == .toggle) continue;
+        const is_down = rl.isKeyDown(key_pair.kb);
+        const has_changed = (key_pair.state == .kb) != is_down;
+        key_pair.state = if (is_down) .kb else .off;
+        if (has_changed) {
+            audio.lock();
+            defer audio.unlock();
+            if (is_down) {
+                if (audio.addOrModifySound(key_pair.note, .{
+                    .waveform = self.current_wave,
+                    .frequency = key_pair.note.to_frequency(0),
+                })) |sound| {
+                    sound.fade = .fade_in;
+                }
+            } else audio.removeSound(key_pair.note);
+        }
+    }
+
+    if (rl.isMouseButtonPressed(.left)) blk: {
+        const now = std.time.milliTimestamp();
+        const click_delay = now - self.last_click;
+        self.last_click = now;
+
+        const mouse_pos = rl.getMousePosition();
+        const item = self.grid.clickedItem(mouse_pos) orelse {
+            self.last_clicked = null;
+            break :blk;
+        };
+        if (click_delay < self.double_click_delay and self.last_clicked != null and self.last_clicked.? == item.key) {
+            audio.mutex.lock();
+            defer audio.mutex.unlock();
+            const is_pressed = item.is_pressed;
+            item.is_pressed = !is_pressed;
+
+            if (!is_pressed) {
+                if (audio.addOrModifySound(item.key, .{
+                    .frequency = item.key.to_frequency(item.octave),
+                    .waveform = self.current_wave,
+                })) |sound| {
+                    sound.fade = .fade_in;
+                    sound.frequency = item.key.to_frequency(item.octave);
+                    sound.waveform = self.current_wave;
+                }
+                self.key_map[@intFromEnum(item.key)].state = .toggle;
+            } else {
+                audio.removeSound(item.key);
+                self.key_map[@intFromEnum(item.key)].state = .off;
+            }
+            self.last_clicked = null;
+        } else self.last_clicked = item.key;
+    }
+    if (rl.isMouseButtonPressed(.right)) blk: {
+        const mouse_pos = rl.getMousePosition();
+        const item = self.grid.clickedItem(mouse_pos) orelse break :blk;
+        audio.lock();
+        defer audio.unlock();
+        if (item.octave < 3) item.octave += 1 else item.octave = -2;
+        if (audio.modifySound(item.key)) |sound| {
+            sound.frequency = item.key.to_frequency(item.octave);
+            sound.waveform = self.current_wave;
+        }
+    }
+
+    for (&self.grid.keys) |*item| {
+        item.is_pressed = self.key_map[@intFromEnum(item.key)].state != .off;
+    }
+}
+
+pub fn draw(self: *App) void {
+    self.grid.draw();
+    draw_current_wave(self.current_wave);
+    draw_volume(self.audio.volume);
+}
+
+fn draw_current_wave(current_wave: Audio.Wave) void {
+    var text_buf: [64]u8 = undefined;
+    const text = std.fmt.bufPrintZ(&text_buf, "Current wave: {s}", .{@tagName(current_wave)}) catch @panic("Unexpected: Format failed");
+    rl.drawText(text, 4, 4, 16, .black);
+}
+
+fn draw_volume(volume: f32) void {
+    var text_buf: [64]u8 = undefined;
+    const intvolume: u8 = @intFromFloat(volume * 100.0);
+    const text = std.fmt.bufPrintZ(&text_buf, "Volume: {}", .{intvolume}) catch @panic("Unexpected: Format failed");
+    rl.drawText(text, 4, 24, 16, .black);
+}
--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 const notes = @import("notes.zig");
 const TonnetzGrid = @import("TonnetzGrid.zig");
 const Audio = @import("Audio.zig");
+const App = @import("App.zig");
 
 comptime {
     _ = TonnetzGrid;
@@ -19,147 +20,36 @@ pub fn main() !void {
 
     rl.initAudioDevice();
     defer rl.closeAudioDevice();
-
-    const audio = try Audio.init();
-    defer audio.deinit();
-    var current_wave: Audio.Wave = .sine;
-
     rl.setTargetFPS(60);
 
-    var last_click: i64 = 0;
-    var last_clicked: ?notes.Key = null;
-    const double_click_delay: i64 = 500;
-
-    var grid = TonnetzGrid.init(.{
-        .color_map = .init(.{
-            .c = .red,
-            .db = .orange,
-            .d = .yellow,
-            .eb = .green,
-            .e = .blue,
-            .f = .purple,
-            .@"f#_gb" = .pink,
-            .g = .brown,
-            .ab = .gray,
-            .a = .black,
-            .bb = .white,
-            .b = rl.Color.init(0xFF, 0xA5, 0x00, 0xFF), // orange
+    var app = App{
+        .grid = TonnetzGrid.init(.{
+            .color_map = .init(.{
+                .c = .red,
+                .db = .orange,
+                .d = .yellow,
+                .eb = .green,
+                .e = .blue,
+                .f = .purple,
+                .@"f#_gb" = .pink,
+                .g = .brown,
+                .ab = .gray,
+                .a = .black,
+                .bb = .white,
+                .b = rl.Color.init(0xFF, 0xA5, 0x00, 0xFF), // orange
+            }),
         }),
-    });
-    var key_map = [_]struct { kb: rl.KeyboardKey, note: notes.Key, state: enum { off, kb, toggle } = .off }{
-        .{ .kb = .z, .note = .c },
-        .{ .kb = .s, .note = .db },
-        .{ .kb = .x, .note = .d },
-        .{ .kb = .d, .note = .eb },
-        .{ .kb = .c, .note = .e },
-        .{ .kb = .v, .note = .f },
-        .{ .kb = .g, .note = .@"f#_gb" },
-        .{ .kb = .b, .note = .g },
-        .{ .kb = .h, .note = .ab },
-        .{ .kb = .n, .note = .a },
-        .{ .kb = .j, .note = .bb },
-        .{ .kb = .m, .note = .b },
+        .audio = try Audio.init(),
+        .current_wave = .sine,
     };
+    defer app.deinit();
 
     while (!rl.windowShouldClose()) {
-        if (rl.isKeyPressed(.w)) {
-            current_wave = switch (current_wave) {
-                .sine => .square,
-                .square => .triangle,
-                .triangle => .sawtooth,
-                .sawtooth => .sine,
-            };
-        }
-
-        for (&key_map) |*key_pair| {
-            if (key_pair.state == .toggle) continue;
-            const is_down = rl.isKeyDown(key_pair.kb);
-            const has_changed = (key_pair.state == .kb) != is_down;
-            key_pair.state = if (is_down) .kb else .off;
-            if (has_changed) {
-                audio.lock();
-                defer audio.unlock();
-                if (is_down) {
-                    if (audio.addOrModifySound(key_pair.note, .{
-                        .waveform = current_wave,
-                        .frequency = key_pair.note.to_frequency(0),
-                    })) |sound| {
-                        sound.fade = .fade_in;
-                    }
-                } else audio.removeSound(key_pair.note);
-            }
-        }
-
-        if (rl.isMouseButtonPressed(.left)) blk: {
-            const now = std.time.milliTimestamp();
-            const click_delay = now - last_click;
-            last_click = now;
-
-            const mouse_pos = rl.getMousePosition();
-            const item = grid.clickedItem(mouse_pos) orelse {
-                last_clicked = null;
-                break :blk;
-            };
-            if (click_delay < double_click_delay and last_clicked != null and last_clicked.? == item.key) {
-                audio.mutex.lock();
-                defer audio.mutex.unlock();
-                const is_pressed = item.is_pressed;
-                item.is_pressed = !is_pressed;
-
-                if (!is_pressed) {
-                    if (audio.addOrModifySound(item.key, .{
-                        .frequency = item.key.to_frequency(item.octave),
-                        .waveform = current_wave,
-                    })) |sound| {
-                        sound.fade = .fade_in;
-                        sound.frequency = item.key.to_frequency(item.octave);
-                        sound.waveform = current_wave;
-                    }
-                    key_map[@intFromEnum(item.key)].state = .toggle;
-                } else {
-                    audio.removeSound(item.key);
-                    key_map[@intFromEnum(item.key)].state = .off;
-                }
-                last_clicked = null;
-            } else last_clicked = item.key;
-        }
-        if (rl.isMouseButtonPressed(.right)) blk: {
-            const mouse_pos = rl.getMousePosition();
-            const item = grid.clickedItem(mouse_pos) orelse break :blk;
-            audio.lock();
-            defer audio.unlock();
-            if (item.octave < 3) item.octave += 1 else item.octave = -2;
-            if (audio.modifySound(item.key)) |sound| {
-                sound.frequency = item.key.to_frequency(item.octave);
-                sound.waveform = current_wave;
-            }
-        }
-
-        for (&grid.keys) |*item| {
-            item.is_pressed = key_map[@intFromEnum(item.key)].state != .off;
-        }
-
+        app.handleInput();
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(.white);
-
-        grid.draw();
-        draw_current_wave(current_wave);
-        draw_volume(audio.volume);
-        // break;
+        app.draw();
     }
-}
-
-fn draw_current_wave(current_wave: Audio.Wave) void {
-    var text_buf: [64]u8 = undefined;
-    const text = std.fmt.bufPrintZ(&text_buf, "Current wave: {s}", .{@tagName(current_wave)}) catch @panic("Unexpected: Format failed");
-    rl.drawText(text, 4, 4, 16, .black);
-}
-
-fn draw_volume(volume: f32) void {
-    var text_buf: [64]u8 = undefined;
-    const intvolume: u8 = @intFromFloat(volume * 100.0);
-    const text = std.fmt.bufPrintZ(&text_buf, "Volume: {}", .{intvolume}) catch @panic("Unexpected: Format failed");
-    rl.drawText(text, 4, 24, 16, .black);
 }
